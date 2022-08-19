ec95db6065d2623928eb52fb4d012a3d5fc4b11a
Vaqif <taqiyevv937@gmail.com>
2022-08-20 00:01:31 +0400

    new file

---
+++ src/main/kotlin/kotlinLesson3/problem_1610.kt
@@ -0,0 +1,17 @@
+package kotlinLesson3
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Qefeslerin sayini daxil edin: ")
+    val qefeslerin_sayi = scan.nextInt()
+    print("Dovsanlarin sayini daxil edin: ")
+    val dovsanlarin_sayi = scan.nextInt()
+
+    if (qefeslerin_sayi<dovsanlarin_sayi){
+        print("Max dovsan sayi: ${qefeslerin_sayi} ")
+    }
+
+
+}
