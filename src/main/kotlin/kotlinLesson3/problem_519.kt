ea99710797ef01268fe66bfa26aa81c7c27d8301
Vaqif <taqiyevv937@gmail.com>
2022-08-20 00:02:53 +0400

    new file

---
+++ src/main/kotlin/kotlinLesson3/problem_519.kt
@@ -0,0 +1,15 @@
+package kotlinLesson3
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter a: ")
+    val a = scan.nextInt()
+    print("Enter b: ")
+    val  b = scan.nextInt()
+    print((a*a) + (b*b))
+
+
+
+}
