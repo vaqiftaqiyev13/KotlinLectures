937d8a4dedaae9ebb7af5ec945ca9d65e73197c0
Vaqif <taqiyevv937@gmail.com>
2022-08-20 00:21:20 +0400

    new file

---
+++ src/main/kotlin/kotlinLesson3/problem_7460.kt
@@ -0,0 +1,14 @@
+package kotlinLesson3
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter number of boy's: ")
+    val n = scan.nextInt()
+    print("Enter number of girl's: ")
+    val m = scan.nextInt()
+    print("Yerlerin sayini daxil et: ")
+    val k = scan.nextInt()
+    print("Otaqlarin sayi: ${(n/k)+(m/k)}")
+}
