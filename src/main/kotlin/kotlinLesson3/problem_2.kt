11daecb2cfd343313cb21d76e7e4b3a5db8f11b1
Vaqif <taqiyevv937@gmail.com>
2022-08-19 23:25:38 +0400

    new file

---
+++ src/main/kotlin/kotlinLesson3/problem_2.kt
@@ -0,0 +1,10 @@
+package kotlinLesson3
+
+import java.util.*
+
+fun main(){
+ val scan = Scanner(System.`in`)
+ print("Enter a number: ")
+ val num = scan.nextInt()
+ println(num.toString().length)
+}
