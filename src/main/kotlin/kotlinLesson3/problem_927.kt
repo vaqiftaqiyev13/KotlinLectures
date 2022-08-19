f57e9517b67477f8fa4f9bd1429ccf82971f03a9
Vaqif <taqiyevv937@gmail.com>
2022-08-20 00:50:33 +0400

    new file

---
+++ src/main/kotlin/kotlinLesson3/problem_927.kt
@@ -0,0 +1,31 @@
+package kotlinLesson3
+
+import java.util.Scanner
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    var umumi_say = 0
+    print("Novlerin sayi: ")
+    val nov_say = scan.nextInt()
+
+    for (n in 1..nov_say){
+        print("sayi yazin: ")
+        val say = scan.nextInt()
+        print("qiymeti yazin: ")
+        val qiymet = scan.nextDouble()
+
+        if (qiymet<50.00){
+            umumi_say+=say
+            print(umumi_say)
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
