032a9b760e1560baabd384e24204cf412358b9ae
Vaqif <taqiyevv937@gmail.com>
2022-08-22 20:24:24 +0400

    new file

---
+++ src/main/kotlin/kotlinLesson3/problem_1607.kt
@@ -0,0 +1,17 @@
+package kotlinLesson3
+
+import java.util.Scanner
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter a number: ")
+    val num = scan.nextLong()
+
+   /* for (it in num.toString().reversed()){
+        print(it)
+    }
+*/
+
+
+
+}
